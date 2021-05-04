7a2f92e5a5c2735148b787c569d70170d09db91f
Bret Gourdie <BretGourdie@gmail.com>
2021-05-03 22:33:31 -0400

    Solved Day03
    Squashed commit of the following:
    
    commit 7663281bc516deac1b178ecc40c9fc04e66a7c22
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Mon May 3 22:32:52 2021 -0400
    
        Solved Day03.2
    
    commit 435e12d11add969378129dd3435ef4a6afddaed9
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Mon May 3 21:51:46 2021 -0400
    
        Solved Day03.1

---
+++ advent-of-code-2020/Day03/Solution.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2020.Day03
+{
+    class Solution : AdventSolution<string>
+    {
+        private const char tree = '#';
+
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+            traverseWithAllToboggans(
+                inputData,
+                new Toboggan[]
+                {
+                    new Toboggan(3, 1)
+                }
+            );
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            traverseWithAllToboggans(
+                inputData,
+                new Toboggan[]
+                {
+                    new Toboggan(1, 1),
+                    new Toboggan(3, 1),
+                    new Toboggan(5, 1),
+                    new Toboggan(7, 1),
+                    new Toboggan(1, 2)
+                }
+            );
+        }
+
+        private void traverseWithAllToboggans(
+            IList<string> inputData,
+            IList<Toboggan> toboggans)
+        {
+            var trees = new List<int>();
+
+            foreach (var toboggan in toboggans)
+            {
+                trees.Add(
+                    traverseWithOneToboggan(
+                        inputData,
+                        toboggan
+                    )
+                );
+            }
+
+            long treesProduct = 1;
+
+            foreach (var tree in trees)
+            {
+                treesProduct *= tree;
+            }
+
+            Console.WriteLine($"Encountered the product of {treesProduct} trees");
+        }
+
+        private int traverseWithOneToboggan(
+            IList<string> inputData,
+            Toboggan toboggan)
+        {
+            int numberOfTrees = 0;
+            int xLocation = 0;
+            int yLocation = 0;
+            string row = inputData.First();
+
+            while (yLocation + toboggan.Y < inputData.Count)
+            {
+                xLocation = (xLocation + toboggan.X) % row.Length;
+                yLocation += toboggan.Y;
+
+                row = inputData[yLocation];
+
+                if (row[xLocation] == tree)
+                {
+                    numberOfTrees += 1;
+                }
+            }
+
+            return numberOfTrees;
+        }
+    }
+}
---
+++ advent-of-code-2020/Day03/Toboggan.cs
@@ -0,0 +1,16 @@
+﻿namespace advent_of_code_2020.Day03
+{
+    class Toboggan
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Toboggan(
+            int x,
+            int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
