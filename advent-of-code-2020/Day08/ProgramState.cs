853ca0676ea7d3f863d65b8c511d018abca95a21
Bret Gourdie <BretGourdie@gmail.com>
2021-05-06 23:43:14 -0400

    Solve Day08
    Squashed commit of the following:
    
    commit b3bbd6a81a8248e1856cbd3c46982a3e679b7a60
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Thu May 6 23:42:55 2021 -0400
    
        Solve Day08

---
+++ advent-of-code-2020/Day08/ProgramState.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2020.Day08
+{
+    class ProgramState
+    {
+        private int accumulator = 0;
+        private int currentInstruction = 0;
+
+        private IList<string> instructions;
+        private ISet<int> executedInstructions;
+        private IDictionary<string, Action<int>> instructionToMethod;
+
+        public ProgramState(IList<string> instructions)
+        {
+            this.instructions = instructions;
+            this.executedInstructions = new SortedSet<int>();
+
+            this.instructionToMethod = new Dictionary<string, Action<int>>();
+            this.instructionToMethod.Add("acc", accumulate);
+            this.instructionToMethod.Add("jmp", jump);
+            this.instructionToMethod.Add("nop", noop);
+        }
+
+        public int ExecuteInstructions()
+        {
+            while (!isStuck() && !IsFinished())
+            {
+                executeInstruction(instructions[currentInstruction]);
+            }
+
+            return accumulator;
+        }
+
+        private bool isStuck()
+        {
+            return executedInstructions.Contains(currentInstruction);
+        }
+
+        public bool IsFinished()
+        {
+            return currentInstruction >= instructions.Count;
+        }
+
+        private void executeInstruction(string line)
+        {
+            executedInstructions.Add(currentInstruction);
+
+            var instructionAndArgument = line.Split(' ');
+            instructionToMethod[instructionAndArgument[0]].Invoke(int.Parse(instructionAndArgument[1]));
+        }
+
+        private void accumulate(int argument)
+        {
+            accumulator += argument;
+            currentInstruction += 1;
+        }
+
+        private void jump(int argument)
+        {
+            currentInstruction += argument;
+        }
+
+        private void noop(int argument)
+        {
+            currentInstruction += 1;
+        }
+    }
+}
---
+++ advent-of-code-2020/Day08/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace advent_of_code_2020.Day08
+{
+    class Solution : AdventSolution
+    {
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+            var accumulator = new ProgramState(inputData).ExecuteInstructions();
+            Console.WriteLine($"The accumulator at the loop is {accumulator}");
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            string[] exchangableInstructions = new []{"jmp", "nop"};
+
+            for (int ii = 0; ii < inputData.Count; ii++)
+            {
+                string line = inputData[ii];
+
+                for (int jj = 0; jj < exchangableInstructions.Length; jj++)
+                {
+                    string oldInstruction = exchangableInstructions[jj];
+
+                    if (line.Contains(oldInstruction))
+                    {
+                        var copyOfInstructions = new List<string>(inputData);
+                        string newInstruction = exchangableInstructions[(jj + 1) % exchangableInstructions.Length];
+                        copyOfInstructions[ii] = line.Replace(oldInstruction, newInstruction);
+
+                        var programState = new ProgramState(copyOfInstructions);
+                        var accumulator = programState.ExecuteInstructions();
+
+                        if (programState.IsFinished())
+                        {
+                            Console.WriteLine($"The accumulator at the finish is {accumulator}");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
