523265f7c6acbd22448a7fc862c532305994422d
Bret Gourdie <BretGourdie@gmail.com>
2021-05-16 21:58:22 -0400

    Solve Day15
    Squashed commit of the following:
    
    commit ba971b8f0afb0de8c88245a5f014faa64766d130
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Sun May 16 21:56:47 2021 -0400
    
        Clear up memory game parameter names
    
    commit 9ac39a3355968934835e514a6bb2ad3a29761b1d
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Sun May 16 21:51:30 2021 -0400
    
        Use RecordSpoken instead of round in constructor
    
    commit fe808d4fbe90a4578567259c1b2f09f15872c5b9
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Sun May 16 21:49:39 2021 -0400
    
        Solve Day15.2
    
    commit 1d1ba8b55fc55211e2b3e71f1a6a0f6871144f32
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Sun May 16 21:42:37 2021 -0400
    
        Solve Day15.1

---
+++ advent-of-code-2020/Day15/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_of_code_2020.Day15
+{
+    class Solution : AdventSolution
+    {
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+            performMemoryGame(inputData, 2020);
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            performMemoryGame(inputData, 30000000);
+        }
+
+        private void performMemoryGame(IList<string> startingNumbers, int totalNumbersToSpeak)
+        {
+            var numberInMemory = new Dictionary<int, SpokenNumber>();
+            int numberLastSpoken = 0;
+
+            var splitInput = startingNumbers.Single().Split(',');
+
+            int ii;
+            for (ii = 1; ii <= splitInput.Length; ii++)
+            {
+                numberLastSpoken = speakStartingNumber(
+                    numberInMemory,
+                    int.Parse(splitInput[ii - 1]),
+                    ii);
+            }
+
+            for (; ii <= totalNumbersToSpeak; ii++)
+            {
+                numberLastSpoken = speakNumber(
+                    numberInMemory,
+                    numberLastSpoken,
+                    ii);
+            }
+
+            Console.WriteLine($"Number last spoken is {numberLastSpoken}");
+        }
+
+        private int speakStartingNumber(
+            IDictionary<int, SpokenNumber> numberInMemory,
+            int number,
+            int round)
+        {
+            numberInMemory[number] = new SpokenNumber(number);
+            numberInMemory[number].RecordSpoken(round);
+            return number;
+        }
+
+        private int speakNumber(
+            IDictionary<int, SpokenNumber> numberInMemory,
+            int numberFromLastRound,
+            int round)
+        {
+            var lastRoundSpokenNumber = numberInMemory[numberFromLastRound];
+
+            var currentNumber = lastRoundSpokenNumber.GetNextSpokenNumber();
+
+            if (!numberInMemory.ContainsKey(currentNumber))
+            {
+                numberInMemory[currentNumber] = new SpokenNumber(currentNumber);
+            }
+
+            numberInMemory[currentNumber].RecordSpoken(round);
+
+            return currentNumber;
+        }
+    }
+}
---
+++ advent-of-code-2020/Day15/SpokenNumber.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace advent_of_code_2020.Day15
+{
+    class SpokenNumber
+    {
+        private readonly Stack<int> spokenOn;
+        public readonly int Number;
+
+        public SpokenNumber(
+            int number)
+        {
+            spokenOn = new Stack<int>();
+            Number = number;
+        }
+
+        public void RecordSpoken(int round)
+        {
+            spokenOn.Push(round);
+        }
+
+        public int GetNextSpokenNumber()
+        {
+            if (spokenOn.Count <= 1)
+            {
+                return 0;
+            }
+
+            else
+            {
+                var mostRecentRoundSpoken = spokenOn.Pop();
+                var secondMostRecentRoundSpoken = spokenOn.Pop();
+
+                spokenOn.Push(secondMostRecentRoundSpoken);
+                spokenOn.Push(mostRecentRoundSpoken);
+
+                return mostRecentRoundSpoken - secondMostRecentRoundSpoken;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Number}: Spoken {spokenOn.Peek()}";
+        }
+    }
+}
