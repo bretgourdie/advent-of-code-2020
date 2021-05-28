3f4c09676beee4ea9edcce9bd9466eda30f03a3c
Bret Gourdie <BretGourdie@gmail.com>
2021-05-28 00:32:08 -0400

    Prove GetEdge() works

--- advent-of-code-2020/Day20/TileSorter.cs
+++ advent-of-code-2020/Day20/TileSorter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace advent_of_code_2020.Day20
 {
@@ -7,6 +8,20 @@ namespace advent_of_code_2020.Day20
     {
         public long Sort(IList<Tile> tiles)
         {
+            int length = tiles.First().Length;
+
+            foreach (var tile in tiles)
+            {
+                var topSide = tile.GetEdge(new Up(length), Reflection.NoReflection);
+                var leftSide = tile.GetEdge(new Left(length), Reflection.NoReflection);
+                var rightSide = tile.GetEdge(new Right(length), Reflection.NoReflection);
+                var bottomSide = tile.GetEdge(new Down(length), Reflection.NoReflection);
+
+                var topReflected = tile.GetEdge(new Up(length), Reflection.Flip);
+                var leftReflected = tile.GetEdge(new Left(length), Reflection.Flip);
+                var rightReflected = tile.GetEdge(new Right(length), Reflection.Flip);
+                var bottomReflected = tile.GetEdge(new Down(length), Reflection.Flip);
+            }
             throw new NotImplementedException();
         }
 
