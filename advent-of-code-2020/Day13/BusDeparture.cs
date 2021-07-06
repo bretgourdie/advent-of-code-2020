0f75fb45f60ab16d25a17c248e0209b3c7b6ed0d
Bret Gourdie <BretGourdie@gmail.com>
2021-07-05 23:02:06 -0400

    Solve Day 13
    Squashed commit of the following:
    
    commit 2c77c2b9432fda88d42f27fb3f497fea19b16834
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Mon Jul 5 23:01:22 2021 -0400
    
        Solve Day 13.2
    
    commit b57bf8dcbe44f308a80e033415a259bc79653573
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Tue Jun 15 17:15:05 2021 -0400
    
        Use largest delta for iterating; still too slow
    
    commit b89de972d09c8fa5b897dbce2c8d7aaeb48c2783
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Tue Jun 15 13:15:10 2021 -0400
    
        Only evaluate timestamps where S[0] % T == 0
    
    commit dc34b2f9efaea858d5fc676029dfbb29174ea725
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Mon Jun 14 17:36:31 2021 -0400
    
        Part 2 attempt; too slow and didn't finish
    
    commit 3a899f49766ceaaf944b4897cbb1d08dba2573ed
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Mon Jun 14 13:35:51 2021 -0400
    
        Use an easier-to-understand algorithm
    
    commit 4d5bde40e2a50c01d4e0151db02121b330455015
    Author: Bret Gourdie <BretGourdie@gmail.com>
    Date:   Mon Jun 14 01:06:47 2021 -0400
    
        Solve Day 13.1

---
+++ advent-of-code-2020/Day13/BusDeparture.cs
@@ -0,0 +1,19 @@
+﻿namespace advent_of_code_2020.Day13
+{
+    class BusDeparture
+    {
+        public readonly int Id;
+
+        protected BusDeparture() { }
+
+        public BusDeparture(int id)
+        {
+            Id = id;
+        }
+
+        public virtual bool TimeStampWorks(long baseTimeStamp, int indexOffset)
+        {
+            return (baseTimeStamp + indexOffset) % Id == 0;
+        }
+    }
+}
---
+++ advent-of-code-2020/Day13/FlexibleBusDeparture.cs
@@ -0,0 +1,12 @@
+﻿namespace advent_of_code_2020.Day13
+{
+    class FlexibleBusDeparture : BusDeparture
+    {
+        public FlexibleBusDeparture() : base() { }
+
+        public override bool TimeStampWorks(long baseTimeStamp, int indexOffset)
+        {
+            return true;
+        }
+    }
+}
