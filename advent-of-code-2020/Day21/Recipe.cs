28535df0c5750c1e6b4aacff0b06605dba57f912
Bret Gourdie <BretGourdie@gmail.com>
2022-03-09 23:44:26 -0500

    Add recipe parsing

---
+++ advent-of-code-2020/Day21/Recipe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace advent_of_code_2020.Day21
+{
+    class Recipe
+    {
+        public readonly string[] Ingredients;
+        public readonly string[] Allergens;
+        public Recipe(string line)
+        {
+            var theSplit = line.Split(new [] {" ("}, StringSplitOptions.RemoveEmptyEntries);
+
+            Ingredients = theSplit[0].Split(' ');
+            if (theSplit.Length > 1)
+            {
+                Allergens = handleAllergens(theSplit[1]);
+            }
+
+            else
+            {
+                Allergens = new string[0];
+            }
+        }
+
+        private string[] handleAllergens(string allergenSegment)
+        {
+            var list = new List<string>();
+            var allergens = allergenSegment.Split(new [] {", "}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var allergen in allergens)
+            {
+                list.Add(
+                    allergen
+                        .Replace(")", "")
+                        .Replace("contains ", ""));
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ advent-of-code-2020/Day21/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2020.Day21
+{
+    class Solution : AdventSolution
+    {
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+            var recipes = new List<Recipe>();
+            foreach (var line in inputData)
+            {
+                recipes.Add(new Recipe(line));
+            }
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
