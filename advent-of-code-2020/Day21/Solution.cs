a51ccfe8544aafe53660021c01e6013eba92d9a9
Bret Gourdie <BretGourdie@gmail.com>
2022-03-17 00:32:23 -0400

    Solved Day21.1 with refactor

--- advent-of-code-2020/Day21/Solution.cs
+++ advent-of-code-2020/Day21/Solution.cs
@@ -8,33 +8,70 @@ namespace advent_of_code_2020.Day21
     {
         protected override void performWorkForProblem1(IList<string> inputData)
         {
+            var recipes = getRecipes(inputData);
+            var nonAllergenicIngredients = getNonAllergenicIngredients(recipes);
+            var nonAllergenicIngredientsUsedCount = countNonAllergenicIngredientsUsedInRecipes(
+                recipes,
+                nonAllergenicIngredients);
+
+            Console.WriteLine(
+                $"Non-allergenic ingredients appear {nonAllergenicIngredientsUsedCount} time(s)");
+        }
+
+        private int countNonAllergenicIngredientsUsedInRecipes(
+            IList<Recipe> recipes,
+            IEnumerable<string> nonAllergenicIngredients)
+        {
+            int count = 0;
+
+            foreach (var recipe in recipes)
+            {
+                foreach (var ingredient in nonAllergenicIngredients)
+                {
+                    if (recipe.Ingredients.Contains(ingredient))
+                    {
+                        count += 1;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private IList<Recipe> getRecipes(IList<string> inputData)
+        {
             var recipes = new List<Recipe>();
             foreach (var line in inputData)
             {
                 recipes.Add(new Recipe(line));
             }
 
+            return recipes;
+        }
+
+        private IEnumerable<string> getNonAllergenicIngredients(IList<Recipe> recipes)
+        {
             var allergens = recipes.SelectMany(x => x.Allergens).Distinct();
 
             var ingredientsThatCannotBeAllergens = getAllIngredients(recipes);
 
             foreach (var allergen in allergens)
             {
-                var ingredients = getAllIngredients(recipes);
+                var ingredientsThatAreNotAllergens = getAllIngredients(recipes);
 
-                var ingredientSets = recipes
+                var ingredientsInAnAllergenRecipe = recipes
                     .Where(x => x.Allergens.Contains(allergen))
                     .Select(x => x.Ingredients);
 
-                foreach (var ingredientSet in ingredientSets)
+                foreach (var ingredientsThatCouldBeAllergens in ingredientsInAnAllergenRecipe)
                 {
-                    ingredients = ingredients.Intersect(ingredientSet);
+                    ingredientsThatAreNotAllergens = ingredientsThatAreNotAllergens.Intersect(ingredientsThatCouldBeAllergens);
                 }
 
-                ingredientsThatCannotBeAllergens = ingredientsThatCannotBeAllergens.Except(ingredients);
+                ingredientsThatCannotBeAllergens = ingredientsThatCannotBeAllergens.Except(ingredientsThatAreNotAllergens);
             }
 
-            var listedIngredients = ingredientsThatCannotBeAllergens.ToList();
+            return ingredientsThatCannotBeAllergens;
         }
 
         protected override void performWorkForProblem2(IList<string> inputData)
