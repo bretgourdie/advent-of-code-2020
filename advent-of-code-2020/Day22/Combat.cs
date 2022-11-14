c3f8c1c6d34af61b5d2131b658a0fc92c112423d
Bret Gourdie <BretGourdie@gmail.com>
2022-11-14 00:57:35 -0500

    Break out rules
    May need a concept of a "game"

---
+++ advent-of-code-2020/Day22/Combat.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace advent_of_code_2020.Day22
+{
+    class Combat : ICombatRules
+    {
+
+        public bool KeepPlayingGame(PlayerDeck[] decks) => decks.All(deck => deck.HasCards());
+
+        public void EvaluateRound(PlayerDeck[] decks)
+        {
+            var cards = decks.Select(x => x.Draw()).ToList();
+
+            int bestIndex = 0;
+            for (int cardIndex = bestIndex + 1; cardIndex < cards.Count; cardIndex++)
+            {
+                if (cards[cardIndex] > cards[bestIndex])
+                {
+                    bestIndex = cardIndex;
+                }
+            }
+
+            decks[bestIndex].Add(cards[bestIndex]);
+            decks[bestIndex].Add(cards[(bestIndex + 1) % cards.Count]);
+        }
+    }
+}
---
+++ advent-of-code-2020/Day22/ICombatRules.cs
@@ -0,0 +1,8 @@
+﻿namespace advent_of_code_2020.Day22
+{
+    public interface ICombatRules
+    {
+        bool KeepPlayingGame(PlayerDeck[] decks);
+        void EvaluateRound(PlayerDeck[] decks);
+    }
+}
