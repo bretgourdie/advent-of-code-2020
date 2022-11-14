a3dc801e1043901e2727b73fdbd40bad6684e713
Bret Gourdie <BretGourdie@gmail.com>
2022-11-13 23:00:39 -0500

    Consolidate ShipForwardMove as AbsoluteDirection

--- advent-of-code-2020/Day12/ShipNavigation.cs
+++ advent-of-code-2020/Day12/ShipNavigation.cs
@@ -31,7 +31,7 @@ namespace advent_of_code_2020.Day12
 
         protected override ICommand parseForwardMove(int amount)
         {
-            return new ShipForwardMove(amount);
+            return new AbsoluteDirectionCommand(amount, ship.Direction);
         }
 
         public override int GetManhattanDistance()
