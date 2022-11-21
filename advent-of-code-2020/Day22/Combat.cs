9324921978fe4600b17880a8168b24e6082d37bf
Bret Gourdie <BretGourdie@gmail.com>
2022-11-21 02:04:48 -0500

    Day 22 Part 2 solved, but too slow

--- advent-of-code-2020/Day22/Combat.cs
+++ advent-of-code-2020/Day22/Combat.cs
@@ -1,22 +1,33 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace advent_of_code_2020.Day22
 {
-    class Combat : ICombatRules
+    public class Combat : ICombatRules
     {
 
-        public bool KeepPlayingGame(PlayerDeck[] decks) => decks.All(deck => deck.HasCards());
-        public void EvaluateRound(PlayerDeck[] decks) { }
+        public bool KeepPlayingGame(Deck[] decks) => decks.All(deck => deck.HasCards());
+        public bool ShouldPlaySubGame(Deck[] decks, IList<int> playerCards) => false;
 
-        public bool ShouldPlaySubGame(PlayerDeck[] decks, int[] playerCards) => false;
-        public int GetWinner(PlayerDeck[] decks)
+        public WinningState GetWinningState(Deck[] decks)
         {
             for (int ii = 0; ii < decks.Length; ii++)
             {
-                if (decks[ii].HasCards()) return ii;
+                if (decks[ii].HasCards())
+                {
+                    return new WinningState(
+                        decks[ii],
+                        decks[(ii + 1) % decks.Length]);
+                }
             }
 
-            return -1;
+            throw new ArgumentException("Both decks have cards");
+        }
+
+        public ICombatRules ForSubGame()
+        {
+            throw new InvalidOperationException("Cannot call subgame from Combat");
         }
     }
 }
---
+++ advent-of-code-2020/Day22/Game.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_of_code_2020.Day22
+{
+    public class Game
+    {
+        public int Play(
+            Deck[] playerDecks,
+            ICombatRules combatRules)
+        {
+            var winningState = playGame(playerDecks, combatRules);
+
+            var score = winningState.WinningDeck.GetScore();
+
+            return score;
+        }
+
+        private WinningState playGame(
+            Deck[] decks,
+            ICombatRules combatRules)
+        {
+            while (combatRules.KeepPlayingGame(decks))
+            {
+                var cards = decks.Select(deck => deck.Draw()).ToList();
+
+                int bestPlayer = 0;
+
+                if (combatRules.ShouldPlaySubGame(decks, cards))
+                {
+                    var slicedDecks = sliceDecks(decks, cards);
+
+                    var subgameResult = playGame(slicedDecks, combatRules.ForSubGame());
+
+                    bestPlayer = subgameResult.WinningDeck.PlayerZeroIndex;
+                }
+
+                else
+                {
+                    bestPlayer = getHigherCard(cards);
+                }
+
+                giveCardToWinner(decks, cards, bestPlayer);
+            }
+
+            return combatRules.GetWinningState(decks);
+        }
+
+        private Deck[] sliceDecks(
+            Deck[] decks,
+            IList<int> cards)
+        {
+            var slicedDecks = new Deck[decks.Length];
+
+            for (int ii = 0; ii < decks.Length; ii++)
+            {
+                var slicedDeckCards = decks[ii].Cards().Take(cards[ii]);
+
+                var slicedDeck = new Deck(decks[ii].Player);
+
+                foreach (var slicedDeckCard in slicedDeckCards)
+                {
+                    slicedDeck.Add(slicedDeckCard);
+                }
+
+                slicedDecks[ii] = slicedDeck;
+            }
+
+            return slicedDecks;
+        }
+
+        private void giveCardToWinner(
+            Deck[] decks,
+            IList<int> cards,
+            int bestPlayer)
+        {
+            decks[bestPlayer].Add(cards[bestPlayer]);
+            decks[bestPlayer].Add(cards[(bestPlayer + 1) % cards.Count]);
+        }
+
+        private int getHigherCard(IList<int> cards)
+        {
+            return cards.IndexOf(cards.Max());
+        }
+    }
+}
