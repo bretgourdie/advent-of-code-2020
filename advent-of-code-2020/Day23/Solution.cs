96656e7b9e32ba0ec4022b3fd455630892019a11
Bret Gourdie <BretGourdie@gmail.com>
2023-01-13 13:23:56 -0500

    Begin working on Day23

---
+++ advent-of-code-2020/Day23/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2020.Day23
+{
+    class Solution : AdventSolution
+    {
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
