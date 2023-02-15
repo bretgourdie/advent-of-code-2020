583530e8c60a8f0989cd6ed636f6a3941356e0df
Bret Gourdie <BretGourdie@gmail.com>
2023-02-15 13:19:03 -0500

    Solved Day 25 Part 1

---
+++ advent-of-code-2020/Day25/Encryption.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace advent_of_code_2020.Day25
+{
+    class Encryption
+    {
+        private const long START_VALUE = 1;
+        private const long SUBJECT = 7;
+        private const long DIVISOR = 20201227;
+
+        public long TransformFromSubject(
+            long cardPublicKey,
+            long doorPublicKey)
+        {
+            long cardSecretLoopSize = findLoopSize(cardPublicKey);
+            long doorSecretLoopSize = findLoopSize(doorPublicKey);
+
+            long cardEncryptionKey = findEncryptionKey(doorPublicKey, cardSecretLoopSize);
+            long doorEncryptionKey = findEncryptionKey(cardPublicKey, doorSecretLoopSize);
+
+            if (cardEncryptionKey != doorEncryptionKey)
+            {
+                throw new ApplicationException(
+                    $"Encryption keys don't match; card: \"{cardEncryptionKey}\"; door: \"{doorEncryptionKey}\"");
+            }
+
+            return cardEncryptionKey;
+        }
+
+        private long findLoopSize(
+            long publicKey)
+        {
+            long value = START_VALUE;
+            long loopSize = 0;
+
+            while (value != publicKey)
+            {
+                value *= SUBJECT;
+                value %= DIVISOR;
+                loopSize += 1;
+            }
+
+            return loopSize;
+        }
+
+        private long findEncryptionKey(
+            long subject,
+            long loopSize)
+        {
+            long value = START_VALUE;
+
+            for (long ii = 0; ii < loopSize; ii++)
+            {
+                value *= subject;
+                value %= DIVISOR;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ advent-of-code-2020/Day25/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_of_code_2020.Day25
+{
+    class Solution : AdventSolution
+    {
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+            var encryptionKey = new Encryption().TransformFromSubject(
+                cardPublicKey: long.Parse(inputData.First()),
+                doorPublicKey: long.Parse(inputData.Skip(1).First())
+            );
+
+            Console.WriteLine($"The encryption key established by the handshake is {encryptionKey}");
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
