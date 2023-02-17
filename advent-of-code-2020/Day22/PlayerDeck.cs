0a64d4e02ec66638454972548507ce8487fc0c52
Bret Gourdie <BretGourdie@gmail.com>
2023-02-17 00:38:21 -0500

    Rebase main onto day22

---
+++ advent-of-code-2020/Day22/PlayerDeck.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_of_code_2020.Day22
+{
+    public class PlayerDeck
+    {
+        private readonly int player;
+        private readonly Queue<int> deck;
+
+        public PlayerDeck(
+            int player)
+        {
+            this.player = player;
+            this.deck = new Queue<int>();
+        }
+
+        public void Add(int card)
+        {
+            deck.Enqueue(card);
+        }
+
+        public int Draw()
+        {
+            return deck.Dequeue();
+        }
+
+        public void Win(int winningCard, int losingCard)
+        {
+            deck.Enqueue(winningCard);
+            deck.Enqueue(losingCard);
+        }
+
+        public bool HasCards() => deck.Any();
+
+        public int GetScore()
+        {
+            var deckList = deck.Reverse().ToList();
+
+            int total = 0;
+            for (int ii = 0; ii < deckList.Count; ii++)
+            {
+                total += deckList[ii] * (ii + 1);
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            return $"Player {player}'s deck: " + String.Join(", ", deck);
+        }
+    }
+}
---
+++ advent-of-code-2020/Day22/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_of_code_2020.Day22
+{
+    class Solution : AdventSolution
+    {
+        private const string Player = "Player ";
+
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+            var decks = getPlayerDecks(inputData).ToArray();
+
+            for (int round = 1; decks.All(deck => deck.HasCards()); round++)
+            {
+                var cards = decks.Select(x => x.Draw()).ToList();
+
+                int bestIndex = 0;
+                for (int cardIndex = bestIndex + 1; cardIndex < cards.Count; cardIndex++)
+                {
+                    if (cards[cardIndex] > cards[bestIndex])
+                    {
+                        bestIndex = cardIndex;
+                    }
+                }
+
+                decks[bestIndex].Add(cards[bestIndex]);
+                decks[bestIndex].Add(cards[(bestIndex + 1) % cards.Count]);
+            }
+
+            var score = decks.Where(deck => deck.HasCards()).Single().GetScore();
+
+            Console.WriteLine($"The score is {score}");
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<PlayerDeck> getPlayerDecks(IEnumerable<string> inputData)
+        {
+            PlayerDeck playerDeck = null;
+
+            foreach (var line in inputData)
+            {
+                if (line.StartsWith(Player))
+                {
+                    var playerNumberStr = line.Substring(Player.Length, 1);
+                    playerDeck = new PlayerDeck(int.Parse(playerNumberStr));
+                }
+
+                else if (string.IsNullOrEmpty(line))
+                {
+                    yield return playerDeck;
+                }
+
+                else
+                {
+                    playerDeck.Add(int.Parse(line));
+                }
+            }
+
+            yield return playerDeck;
+        }
+    }
+}
