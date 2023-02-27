4a1bf7e5a86ad7db6c1d340c52513359ee55c704
Bret Gourdie <BretGourdie@gmail.com>
2023-02-26 20:12:44 -0500

    Initial commit

---
+++ advent-of-code-2020/Day20/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2020.Day20
+{
+    class Solution : AdventSolution
+    {
+        protected override void performWorkForProblem1(IList<string> inputData)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void performWorkForProblem2(IList<string> inputData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
